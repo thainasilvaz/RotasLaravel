06f37c7f79a9dd3d508594c265faaa1fceef8cc9
thainasilvaz <thaina.silva254@etec.sp.gov.br>
2023-02-28 09:56:59 -0300

    LaravelRotas

---
+++ Documents/projetoweb_rotas/resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=e, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Home</h1>
+    <a href="produtos">produtos<br></a>
+    <a href="empresa">empresa<br></a>
+    <a href="contato">contato<br></a>
+</body>
+</html>
